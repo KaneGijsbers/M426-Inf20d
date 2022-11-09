8f8249155ae194987f2619667ea944180a07edb9
K.Gijsbers <kane.gijsbers@kauz.ch>
2022-11-09 08:26:00 +0100

    Aufgaben gelöst

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,48 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void Determine_whenNumberNotDividibleByThreeFiveSeven_shouldReturnNumberAsString()
         {
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var number = 4;
+            var expectedResult = "4";
+
+            var result = fooBarQixDeterminer.Determine(number);
+
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void Determine_whenNumberIsDividedByThree_shouldReturnFoo()
+        {
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var number = 3;
+            var expectedResult = "Foo";
+
+            var result = fooBarQixDeterminer.Determine(number);
+
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void Determine_whenNumberIsDividedByFive_shouldReturnBar()
+        {
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var number = 5;
+            var expectedResult = "Bar";
+
+            var result = fooBarQixDeterminer.Determine(number);
+
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void Determine_whenNumberIsDividedBySeven_shouldReturnQix()
+        {
+            var fooBarQixDeterminer = new FooBarQixDeterminer();
+            var number = 7;
+            var expectedResult = "Qix";
+
+            var result = fooBarQixDeterminer.Determine(number);
+
+            Assert.Equal(expectedResult, result);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,7 +1,27 @@
-﻿namespace FooBarQix
+﻿using System.Xml.Schema;
+
+namespace FooBarQix
 {
     public class FooBarQixDeterminer
     {
-
+        public string Determine(int number)
+        {
+            if (number % 3 == 0)
+            {
+                return "Foo";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Bar";
+            }
+            else if (number % 7 == 0)
+            {
+                return "Qix";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
     }
 }
